f56f81fef3ad6064bc33f78e0df75886971e1966
Bouneb Rayan <raynbouneb@gmail.com>
2025-10-26 00:25:35 +0100

    Style: Footer enhancements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,51 +24,51 @@ export default function Footer() {
 
         {/* Découvrez nos meubles */}
         <div>
-          <h3 className="font-bold uppercase text-sm border-b pb-1">DÉCOUVREZ NOS MEUBLES</h3>
+          <h3 className="font-bold uppercase text-sm border-b border-black pb-1">DÉCOUVREZ NOS nouveautés</h3>
           <ul className="mt-2 space-y-1">
-            <li><Link href="/categories/salons" className="hover:underline">Salons</Link></li>
-            <li><Link href="/categories/chambres" className="hover:underline">Chambres à coucher</Link></li>
-            <li><Link href="/categories/salles à manger" className="hover:underline">Salles à manger</Link></li>
-            <li><Link href="/categories/Consoles & meubles d’entrée" className="hover:underline">Consoles & Meubles d’entrée</Link></li>
+            <li><Link href="/categories/salons">Salons</Link></li>
+            <li><Link href="/categories/chambres">Chambres à coucher</Link></li>
+            <li><Link href="/categories/salles à manger">Salles à manger</Link></li>
+            <li><Link href="/categories/Consoles & meubles d’entrée">Consoles & Meubles d’entrée</Link></li>
           </ul>
         </div>
 
         {/* Meublez vos salons */}
         <div>
-          <h3 className="font-bold uppercase text-sm border-b pb-1">MEUBLEZ VOS SALONS</h3>
+          <h3 className="font-bold uppercase text-sm border-b border-black pb-1">MEUBLEZ VOS SALONS</h3>
           <ul className="mt-2 space-y-1">
-            <li><Link href="/categories/canapés & fauteuils" className="hover:underline">Canapés & Fauteuils</Link></li>
-            <li><Link href="/categories/tables basses & tables de coin" className="hover:underline">Tables basses & Tables de coin</Link></li>
-            <li><Link href="/categories/meubles tv" className="hover:underline">Meubles TV</Link></li>
+            <li><Link href="/categories/canapés & fauteuils" >Canapés & Fauteuils</Link></li>
+            <li><Link href="/categories/tables basses & tables de coin" >Tables basses & Tables de coin</Link></li>
+            <li><Link href="/categories/meubles tv">Meubles TV</Link></li>
           </ul>
         </div>
 
         {/* Décorez votre maison */}
         <div >
-          <h3 className="font-bold uppercase text-sm border-b pb-1">DÉCOREZ VOTRE MAISON</h3>
+          <h3 className="font-bold uppercase text-sm border-b border-black pb-1">DÉCOREZ VOTRE MAISON</h3>
           <ul className="mt-2 space-y-1">
-            <li><Link href="/categories/accessoires déco" className="hover:underline">Accessoires Déco</Link></li>
-            <li><Link href="/categories/statues" className="hover:underline">Statues</Link></li>
-            <li><Link href="/categories/vases" className="hover:underline">Vases</Link></li>
-            <li><Link href="/categories/miroirs" className="hover:underline">Miroirs</Link></li>
+            <li><Link href="/categories/accessoires déco">Accessoires Déco</Link></li>
+            <li><Link href="/categories/art-de-la-table">Art de la Table</Link></li>
+            <li><Link href="/categories/luminaires">Luminaires</Link></li>
+            <li><Link href="/categories/miroirs">Miroirs</Link></li>
           </ul>
         </div>
 
         {/* Suivez-nous sur les réseaux sociaux */}
         <div>
-          <h3 className="font-bold uppercase text-sm border-b pb-1">SUIVEZ-NOUS SUR LES RÉSEAUX SOCIAUX</h3>
+          <h3 className="font-bold uppercase text-sm border-b border-black pb-1">SUIVEZ-NOUS SUR LES RÉSEAUX SOCIAUX</h3>
           <ul className="mt-2 flex flex-row lg:flex-col gap-2 items-center lg:items-start justify-center lg:justify-start">
-            <li className="flex items-center gap-2">
+            <li className="flex items-center gap-2 hover:scale-105 transition-all duration-300 cursor-pointer">
               <FaFacebook size={24} />
-              <Link href="https://www.facebook.com/DolceVitaLuxuryHomeCollection" className="hover:underline">Facebook</Link>
+              <Link href="https://www.facebook.com/DolceVitaLuxuryHomeCollection">Facebook</Link>
             </li>
-            <li className="flex items-center gap-2">
+            <li className="flex items-center gap-2 hover:scale-105 transition-all duration-300 cursor-pointer">
               <FaInstagram size={24} />
-              <Link href="https://www.instagram.com/dolce_vita_home_collection" className="hover:underline">Instagram</Link>
+              <Link href="https://www.instagram.com/dolce_vita_home_collection">Instagram</Link>
             </li>
-            <li className="flex items-center gap-2">
+            <li className="flex items-center gap-2 hover:scale-105 transition-all duration-300 cursor-pointer">
               <FaLinkedin size={24} />
-              <Link href="https://www.linkedin.com/company/dolce-vita-home-collection" className="hover:underline">LinkedIn</Link>
+              <Link href="https://www.linkedin.com/company/dolce-vita-home-collection" >LinkedIn</Link>
             </li>
           </ul>
         </div>
